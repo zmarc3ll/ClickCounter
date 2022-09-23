87e0ff055b4787a4b07701700420791b94b8c332
Zuber Marcell <zubermarcellpetrik@gmail.com>
2022-09-23 09:19:31 +0200

    majdnem kész

---
+++ app/src/main/java/hu/petrik/clickcounter/MainActivity.java
@@ -0,0 +1,59 @@
+package hu.petrik.clickcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button felGomb;
+    private TextView szamlalo;
+    private Button leGomb;
+    private int counter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+
+        felGomb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                szamlalo.setText(counter);
+                if (counter<0) {
+                    szamlalo.setTextColor(Color.RED);
+                } else {
+                    szamlalo.setTextColor(Color.GREEN);
+                }
+            }
+        });
+
+        leGomb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter--;
+                szamlalo.setText(counter);
+                if (counter<0) {
+                    szamlalo.setTextColor(Color.RED);
+                } else {
+                    szamlalo.setTextColor(Color.GREEN);
+                }
+            }
+        });
+    }
+
+    public void init(){
+
+        felGomb=(Button) findViewById(R.id.felGomb);
+        szamlalo=(TextView) findViewById(R.id.szamlalo);
+        leGomb=(Button) findViewById(R.id.leGomb);
+        counter=0;
+
+    }
+}
